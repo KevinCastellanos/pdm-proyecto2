50ed4a58c3e3b3d5dc6ba35e50bfe013e1a14b02
cindysoto <64655136+cindysoto@users.noreply.github.com>
2020-06-30 18:48:46 -0600

    Renombré Paquete clases

--- app/src/main/java/sv/edu/ues/fia/eisi/pdm_proyecto2/Interfaces/ApiServices.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/pdm_proyecto2/Interfaces/ApiServices.java
@@ -3,7 +3,7 @@ package sv.edu.ues.fia.eisi.pdm_proyecto2.Interfaces;
 import retrofit2.Call;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
-import sv.edu.ues.fia.eisi.pdm_proyecto2.clases.Usuario;
+import sv.edu.ues.fia.eisi.pdm_proyecto2.Clases.Usuario;
 
 public interface ApiServices {
 
